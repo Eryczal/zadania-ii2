04b32ef33c87eb6d841c0dcada002b3264403574
Eryczal <79263592+Eryczal@users.noreply.github.com>
2022-03-15 12:41:53 +0100

    Add files via upload

---
+++ 03/header.php
@@ -0,0 +1,25 @@
+<!DOCTYPE html>
+<html lang="pl">
+	<head>
+		<title><?php out($page_title); ?></title>
+		<meta charset="utf-8" />
+		<meta name="viewport" content="width=device-width, initial-scale=1, user-scalable=no" />
+        <meta name="description" content="<?php out($page_description); ?>" />
+		<link rel="stylesheet" href="assets/css/main.css" />
+		<noscript><link rel="stylesheet" href="assets/css/noscript.css" /></noscript>
+	</head>
+	<body class="is-preload">
+        
+		<!-- Sidebar -->
+			<section id="sidebar">
+				<div class="inner">
+					<nav>
+						<ul>
+							<li><a href="#intro">Welcome</a></li>
+							<li><a href="#one">Who we are</a></li>
+							<li><a href="#two">What we do</a></li>
+							<li><a href="#three">Kalkulator</a></li>
+						</ul>
+					</nav>
+				</div>
+			</section>
